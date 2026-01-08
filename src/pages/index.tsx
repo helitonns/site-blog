6bd47a5a2e1d146adc35a645dc75a3c1419e8ec7
helitonns <helitonns@gmail.com>
2026-01-08 10:43:23 -0400

    Seção: Hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <div className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}>
-      <div>
-        PÁGINA HOME
-      </div>
-    </div>
+    <>
+      <article className={`flex flex-col min-h-screen bg-zinc-50 dark:bg-black`}>
+        <HeroSection />
+      </article>
+    </>
   );
 }
