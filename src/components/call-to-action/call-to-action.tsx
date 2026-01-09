d4857bd319091bb99589778ce3e519a8a5039afc
helitonns <helitonns@gmail.com>
2026-01-09 09:02:28 -0400

    Seção: Call to action

---
+++ src/components/call-to-action/call-to-action.tsx
@@ -0,0 +1,26 @@
+import { ptSansCaption } from "@/styles/fonts";
+import { ArrowRight, Store } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export function CallToAction(){
+
+  return(
+    <section className="py-22 bg-linear-to-b from-cyan-950/35 to-gray-700 relative">
+      <div className="container">
+        <div className="flex flex-col items-center gap-6 text-center">
+          <div className="p-4 bg-cyan-300 w-fit rounded-full -mt-28">
+            <Store className="text-cyan-100"/>
+          </div>
+          <h2 className={`${ptSansCaption.className} text-gray-100 text-heading-xl text-balance`}>Crie uma loja online e inicie suas vendas ainda hoje</h2>
+          <Button className="w-fit mt-6" asChild>
+            <Link href={"/criar-loja"}>
+              Criar loja grátis
+              <ArrowRight />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+}
