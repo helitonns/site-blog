62953a0389f45d13d7179c8158da936b67432ff0
helitonns <helitonns@gmail.com>
2026-01-11 10:10:50 -0400

    Componete: PostGridCard

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,6 +1,7 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card";
+import { PostGridCard } from "./components/post-grid-card";
 
 export function BlogList(){
   const router = useRouter();
@@ -10,7 +11,7 @@ export function BlogList(){
 
   return(
     <div className="flex flex-col py-24 grow h-full">
-      <header>
+      <header className="pb-14">
         <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
           <div className="flex flex-col gap-4 md:px-0">
             <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
@@ -26,18 +27,25 @@ export function BlogList(){
         </div>
       </header>
 
-      <PostCard 
-        title="Transformando seu negócio em uma loja virtual"
-        description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
-        date="20/12/2024"
-        author={{
-          avatar: "/customer-01.png",
-          name: "Aspen Dokidis"
-        }}
-        image="/assets/primeiro-post.png"
-        slug="trasnsformando-seu-negocio-em-uma-loja-virtual"
-      />
+      <PostGridCard>
+        {Array.from({length: 4}).map((_, index)=> (
+          <PostCard
+            key={`inex-${index}`}
+            title="Transformando seu negócio em uma loja virtual"
+            description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+            date="20/12/2024"
+            author={{
+              avatar: "/customer-01.png",
+              name: "Aspen Dokidis"
+            }}
+            image="/assets/primeiro-post.png"
+            slug="trasnsformando-seu-negocio-em-uma-loja-virtual"
+          />
+        ))}
+      </PostGridCard>
 
     </div>
   );
 }
+
+
---
+++ src/templates/blog/components/post-grid-card/post-grid-card.tsx
@@ -0,0 +1,12 @@
+type PostGridCardProps = {
+  children: React.ReactNode;
+}
+
+export function PostGridCard({ children }: PostGridCardProps){
+
+  return(
+    <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {children}
+    </div>
+  );
+}
