a1b572150cc2082b32ac8cb4b9e3e1961ad04693
helitonns <helitonns@gmail.com>
2026-01-14 19:34:21 -0400

    Melhorando o SEO da Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,29 @@
 import { LadingPage } from "@/templates/landing-page";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Site.Set",
+  description: "Venda seus produtos como afiliadp em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.set",
+    description: "Venda seus produtos como afiliadp em um único lugar",
+    url: 'https://nextjs-fundamentos.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://nextjs-fundamentos.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+
+  }
+
+}
 
 export default function HomePage(){
 
